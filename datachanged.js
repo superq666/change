940f39cced80188f939890d523809805df2d9cd6
daqi <369119614@qq.com>
2018-08-23 12:07:53 +0800

    Initial commit

---
+++ datachanged.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+
+function changeJson(newhost, newcookie){
+    fs.readFile('./AA.json',function(err,data){
+        if(err){
+            console.error(err);
+        }
+        var json = data.toString();
+        console.log(json)
+        json = JSON.parse(json);
+   
+
+        //对host以及cookie进行控制台现实以及修改操作；
+        console.log(json.nodes[1].uri.host);
+        json.nodes[1].uri.host=newhost;
+        console.log(json.nodes[1].uri.host);
+
+        console.log(json.nodes[1].headers[7].value);
+        json.nodes[1].headers[7].value=newcookie;
+        console.log(json.nodes[1].headers[7].value);
+
+
+        getcookies = get("console.log(json.nodes[1].headers[7].value");
+        console.log(getcookies.value);
+
+        //对四个不需要的数据进行删除操作；
+        for(let i=10;i>-1;i--){
+            if(i == 0 || i==6 || i==8|| i==9){
+
+                 json.nodes[1].headers.splice(i,1);
+                 console.log("删除第"+i+"个");
+
+            }
+        } 
+        
+
+        json.total = json.nodes.length;
+        var str = JSON.stringify(json);
+        // console.log(str);
+        fs.writeFile('./new/package.json',str,function(err){
+            if(err){
+                console.error(err);
+            }
+            console.log('--------------修改成功---------------');
+            console.log(json);
+        })
+        // return cookies;
+    })
+}
+changeJson('www.baidu.com', 'www.google.com');//执行一下;
+
+
+
+
